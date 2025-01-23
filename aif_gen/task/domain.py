47879fadf9303dfe33cbe4dc66c000ce22cfef07
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-01-22 21:11:52 -0500

    Integrate DomainComponent into Domain

--- aif_gen/task/domain.py
+++ aif_gen/task/domain.py
@@ -1,17 +1,63 @@
-from typing import Any, Dict
+from typing import Any, Dict, List, Optional
+
+from .domain_component import DomainComponent
 
 
 class Domain:
-    def __init__(self, domain: str) -> None:
-        self._domain = domain
+    r"""A domain is a combination of DomainComponents along with a weight for each component.
+
+    Args:
+        components (List[DomainComponent]): List of DomainComponents that constitute the domain.
+        weights (Optional[List[float]]): Weights given to each constituent component (uniform if not specified).
+    """
+
+    def __init__(
+        self, components: List[DomainComponent], weights: Optional[List[float]] = None
+    ) -> None:
+        if not len(components):
+            raise ValueError(
+                'Cannot initialize a Domain with an empty list of DomainComponents'
+            )
+
+        if weights is None:
+            weights = [1 / len(components)] * len(components)
+
+        if len(weights) != len(components):
+            raise ValueError(
+                f'Number of components and weights must match, but got {len(components)} components and {len(weights)} weights'
+            )
+
+        self._components = components
+        self._weights = weights
 
     @classmethod
-    def from_dict(cls, domain_dict: Dict[str, Any]) -> 'Domain':
-        return cls('Mock Domain')
+    def from_dict(cls, domain_dict: Dict[str, Dict[str, Any]]) -> 'Domain':
+        components, component_weights = [], []
+        for component_name, component_args in domain_dict.items():
+            component_args['name'] = component_name
+            components.append(DomainComponent.from_dict(component_args))
+
+            if 'weight' in component_args:
+                component_weights.append(component_args['weight'])
+
+        weights = None if not len(component_weights) else component_weights
+        return cls(components, weights)
 
     def __str__(self) -> str:
-        return f'Domain: {self._domain}'
+        s = f'Domain: ['
+        for i in range(self.num_components):
+            s += f'({self.components[i]}, weight={self.weights[i]:.2f}), '
+        s += ']'
+        return s
 
     @property
-    def domain(self) -> str:
-        return self._domain
+    def components(self) -> List[DomainComponent]:
+        return self._components
+
+    @property
+    def num_components(self) -> int:
+        return len(self.components)
+
+    @property
+    def weights(self) -> List[float]:
+        return self._weights
--- test/test_alignment_task.py
+++ test/test_alignment_task.py
@@ -4,31 +4,51 @@ from aif_gen.task import AlignmentTask, Domain
 
 
 def test_init():
-    domain = Domain('Mock Domain')
+    component_dict = {
+        'Component A': {
+            'seed_words': ['a_foo', 'a_bar', 'a_baz'],
+            'description': 'A Mock Domain Component',
+        },
+        'Component B': {
+            'seed_words': ['b_foo', 'b_bar', 'b_baz'],
+            'description': 'B Mock Domain Component',
+        },
+    }
+    domain = Domain.from_dict(component_dict)
     objective = 'Mock Objective'
     preference = 'Mock Preference'
 
     task = AlignmentTask(domain, objective, preference)
-    exp_str = 'AlignmentTask(Domain: Mock Domain, Objective: Mock Objective, Preference: Mock Preference)'
+    exp_str = f'AlignmentTask({str(domain)}, Objective: Mock Objective, Preference: Mock Preference)'
     assert str(task) == exp_str
 
 
 def test_init_from_dict():
     task_dict = {
-        'domain': 'Mock Domain',
+        'domain': {
+            'Component A': {
+                'seed_words': ['a_foo', 'a_bar', 'a_baz'],
+                'description': 'A Mock Domain Component',
+            },
+            'Component B': {
+                'seed_words': ['b_foo', 'b_bar', 'b_baz'],
+                'description': 'B Mock Domain Component',
+            },
+        },
         'objective': 'Mock Objective',
         'preference': 'Mock Preference',
     }
 
     task = AlignmentTask.from_dict(task_dict)
-    exp_str = 'AlignmentTask(Domain: Mock Domain, Objective: Mock Objective, Preference: Mock Preference)'
+    domain = Domain.from_dict(task_dict['domain'])
+    exp_str = f'AlignmentTask({str(domain)}, Objective: Mock Objective, Preference: Mock Preference)'
     assert str(task) == exp_str
 
 
 def test_init_from_dict_missing_keys():
-    task_dict = {  # Missing 'preference' key
-        'domain': 'Mock Domain',
+    task_dict = {  # Missing 'domain' key
         'objective': 'Mock Objective',
+        'preference': 'Mock Preference',
     }
 
     with pytest.raises(ValueError):
