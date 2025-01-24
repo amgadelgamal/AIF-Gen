9c261924bb119dbad60525a3f96a0bf391000ead
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-01-24 17:51:57 -0500

    Implement entropy validation

--- aif_gen/dataset/validation/entropy.py
+++ aif_gen/dataset/validation/entropy.py
@@ -1,5 +1,10 @@
+import typing
+from collections import Counter
 from typing import Dict, List
 
+import numpy as np
+
+from aif_gen.dataset import AlignmentDataset, ContinualAlignmentDataset
 from aif_gen.typing import Dataset
 
 
@@ -21,4 +26,47 @@ def entropy_validation(dataset: Dataset) -> List[Dict[str, float]]:
         If the input dataset is an AlignmentDataset (non-continual), this function
         returns a 1 element list with the relevant statistics.
     """
-    raise NotImplementedError()
+    if isinstance(dataset, AlignmentDataset):
+        datasets = [dataset]
+    else:
+        # This assert is here to make mypy happy
+        assert isinstance(dataset, ContinualAlignmentDataset)
+        datasets = dataset.datasets
+
+    results = []
+    for dataset in datasets:
+        results.append(_entropy_validation(dataset))
+    return results
+
+
+def _entropy_validation(dataset: AlignmentDataset) -> Dict[str, float]:
+    token_freq: typing.Counter[str] = Counter()
+    prompts_freq: typing.Counter[str] = Counter()
+    chosen_freq: typing.Counter[str] = Counter()
+    rejected_freq: typing.Counter[str] = Counter()
+
+    for sample in dataset.samples:
+        prompt, chosen, rejected = sample.prompt, sample.chosen, sample.rejected
+
+        token_freq.update(str(sample).split())
+        prompts_freq.update(prompt.split())
+        chosen_freq.update(chosen.split())
+        rejected_freq.update(rejected.split())
+
+    return {
+        'token_entropy': _compute_entropy(token_freq),
+        'prompt_entropy': _compute_entropy(prompts_freq),
+        'chosen_entropy': _compute_entropy(chosen_freq),
+        'rejected_entropy': _compute_entropy(rejected_freq),
+    }
+
+
+def _compute_entropy(word_freq: Counter) -> float:
+    # Normalize frequency counts to probabilities
+    norm_counts = []
+    total = sum(word_freq.values(), 0.0)
+    for word in word_freq:
+        norm_counts.append(word_freq[word] / total)
+
+    norm_counts = np.array(norm_counts)
+    return float(-(norm_counts * np.log(norm_counts)).sum())
--- test/test_validation/test_entropy_validation.py
+++ test/test_validation/test_entropy_validation.py
@@ -1,13 +1,282 @@
-import pytest
-
+from aif_gen.dataset import (
+    AlignmentDataset,
+    AlignmentDatasetSample,
+    ContinualAlignmentDataset,
+)
 from aif_gen.dataset.validation import entropy_validation
 
 
-@pytest.mark.skip(reason='Entropy validation not implemented')
-def test_entropy_validation():
-    _ = entropy_validation()
+def test_entropy_validation_all_unique():
+    samples = [
+        AlignmentDatasetSample(
+            'Mock prompt A 1', 'Winning Response A 1', 'Losing Response A 1'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt B 1', 'Winning Response B 1', 'Losing Response B 1'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt C 1', 'Winning Response C 1', 'Losing Response C 1'
+        ),
+    ]
+    mock_task = None
+    dataset = AlignmentDataset(task=mock_task, samples=samples)
+    expected_entropy = [
+        {
+            'chosen_entropy': 1.660947433286918,
+            'prompt_entropy': 1.660947433286918,
+            'rejected_entropy': 1.660947433286918,
+            'token_entropy': 2.5232109529528914,
+        }
+    ]
+    assert entropy_validation(dataset) == expected_entropy
 
 
-@pytest.mark.skip(reason='Entropy validation not implemented')
+def test_entropy_validation_all_same_prompts():
+    samples = [
+        AlignmentDatasetSample(
+            'Mock prompt A 2', 'Winning Response A 2', 'Losing Response A 2'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt A 2', 'Winning Response B 2', 'Losing Response B 2'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt A 2', 'Winning Response C 2', 'Losing Response C 2'
+        ),
+    ]
+    mock_task = None
+    dataset = AlignmentDataset(task=mock_task, samples=samples)
+    expected_entropy = [
+        {
+            'chosen_entropy': 1.660947433286918,
+            'prompt_entropy': 1.3862943611198906,
+            'rejected_entropy': 1.660947433286918,
+            'token_entropy': 2.5024938932551737,
+        }
+    ]
+    assert entropy_validation(dataset) == expected_entropy
+
+
+def test_entropy_validation_all_same_responses():
+    samples = [
+        AlignmentDatasetSample(
+            'Mock prompt A 3', 'Winning Response A 3', 'Losing Response B 3'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt B 3', 'Winning Response A 3', 'Losing Response B 3'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt C 3', 'Winning Response A 3', 'Losing Response B 3'
+        ),
+    ]
+    mock_task = None
+    dataset = AlignmentDataset(task=mock_task, samples=samples)
+    expected_entropy = [
+        {
+            'chosen_entropy': 1.3862943611198906,
+            'prompt_entropy': 1.660947433286918,
+            'rejected_entropy': 1.3862943611198906,
+            'token_entropy': 2.496481079820755,
+        }
+    ]
+    assert entropy_validation(dataset) == expected_entropy
+
+
+def test_entropy_validation_all_same_everything():
+    samples = [
+        AlignmentDatasetSample(
+            'Mock prompt A 4', 'Winning Response A 4', 'Losing Response A 4'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt A 4', 'Winning Response A 4', 'Losing Response A 4'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt A 4', 'Winning Response A 4', 'Losing Response A 4'
+        ),
+    ]
+    mock_task = None
+    dataset = AlignmentDataset(task=mock_task, samples=samples)
+    expected_entropy = [
+        {
+            'chosen_entropy': 1.3862943611198906,
+            'prompt_entropy': 1.3862943611198906,
+            'rejected_entropy': 1.3862943611198906,
+            'token_entropy': 2.3034884952192693,
+        }
+    ]
+    assert entropy_validation(dataset) == expected_entropy
+
+
+def test_entropy_validation_no_response_entropy():
+    samples = [
+        AlignmentDatasetSample('Mock prompt A 5', 'foo foo foo', 'bar bar bar'),
+        AlignmentDatasetSample('Mock prompt B 5', 'foo foo foo', 'bar bar bar'),
+        AlignmentDatasetSample('Mock prompt C 5', 'foo foo foo', 'bar bar bar'),
+    ]
+    mock_task = None
+    dataset = AlignmentDataset(task=mock_task, samples=samples)
+    expected_entropy = [
+        {
+            'chosen_entropy': -0.0,
+            'prompt_entropy': 1.660947433286918,
+            'rejected_entropy': -0.0,
+            'token_entropy': 2.289294054503374,
+        }
+    ]
+    assert entropy_validation(dataset) == expected_entropy
+
+
+def test_entropy_validation_no_prompt_entropy():
+    samples = [
+        AlignmentDatasetSample(
+            'foo foo foo', 'Winning Response A 6', 'Losing Response C 6'
+        ),
+        AlignmentDatasetSample(
+            'foo foo foo', 'Winning Response B 6', 'Losing Response C 6'
+        ),
+        AlignmentDatasetSample(
+            'foo foo foo', 'Winning Response C 6', 'Losing Response C 6'
+        ),
+    ]
+    mock_task = None
+    dataset = AlignmentDataset(task=mock_task, samples=samples)
+    expected_entropy = [
+        {
+            'chosen_entropy': 1.660947433286918,
+            'prompt_entropy': -0.0,
+            'rejected_entropy': 1.3862943611198906,
+            'token_entropy': 2.465931856301205,
+        }
+    ]
+    assert entropy_validation(dataset) == expected_entropy
+
+
 def test_entropy_countinual_dataset():
-    _ = entropy_validation()
+    samples = [
+        AlignmentDatasetSample(
+            'Mock prompt A 1', 'Winning Response A 1', 'Losing Response A 1'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt B 1', 'Winning Response B 1', 'Losing Response B 1'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt C 1', 'Winning Response C 1', 'Losing Response C 1'
+        ),
+    ]
+    mock_task = None
+    dataset_one = AlignmentDataset(task=mock_task, samples=samples)
+
+    samples = [
+        AlignmentDatasetSample(
+            'Mock prompt A 2', 'Winning Response A 2', 'Losing Response A 2'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt A 2', 'Winning Response B 2', 'Losing Response B 2'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt A 2', 'Winning Response C 2', 'Losing Response C 2'
+        ),
+    ]
+    mock_task = None
+    dataset_two = AlignmentDataset(task=mock_task, samples=samples)
+
+    samples = [
+        AlignmentDatasetSample(
+            'Mock prompt A 3', 'Winning Response A 3', 'Losing Response B 3'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt B 3', 'Winning Response A 3', 'Losing Response B 3'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt C 3', 'Winning Response A 3', 'Losing Response B 3'
+        ),
+    ]
+    mock_task = None
+    dataset_three = AlignmentDataset(task=mock_task, samples=samples)
+
+    samples = [
+        AlignmentDatasetSample(
+            'Mock prompt A 4', 'Winning Response A 4', 'Losing Response A 4'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt A 4', 'Winning Response A 4', 'Losing Response A 4'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt A 4', 'Winning Response A 4', 'Losing Response A 4'
+        ),
+    ]
+    mock_task = None
+    dataset_four = AlignmentDataset(task=mock_task, samples=samples)
+
+    samples = [
+        AlignmentDatasetSample('Mock prompt A 5', 'foo foo foo', 'bar bar bar'),
+        AlignmentDatasetSample('Mock prompt B 5', 'foo foo foo', 'bar bar bar'),
+        AlignmentDatasetSample('Mock prompt C 5', 'foo foo foo', 'bar bar bar'),
+    ]
+    mock_task = None
+    dataset_five = AlignmentDataset(task=mock_task, samples=samples)
+
+    samples = [
+        AlignmentDatasetSample(
+            'foo foo foo', 'Winning Response A 6', 'Losing Response C 6'
+        ),
+        AlignmentDatasetSample(
+            'foo foo foo', 'Winning Response B 6', 'Losing Response C 6'
+        ),
+        AlignmentDatasetSample(
+            'foo foo foo', 'Winning Response C 6', 'Losing Response C 6'
+        ),
+    ]
+    mock_task = None
+    dataset_six = AlignmentDataset(task=mock_task, samples=samples)
+    expected_entropy = []
+
+    dataset = ContinualAlignmentDataset(
+        [
+            dataset_one,
+            dataset_two,
+            dataset_three,
+            dataset_four,
+            dataset_five,
+            dataset_six,
+        ]
+    )
+
+    expected_entropy = [
+        {
+            'chosen_entropy': 1.660947433286918,
+            'prompt_entropy': 1.660947433286918,
+            'rejected_entropy': 1.660947433286918,
+            'token_entropy': 2.5232109529528914,
+        },
+        {
+            'chosen_entropy': 1.660947433286918,
+            'prompt_entropy': 1.3862943611198906,
+            'rejected_entropy': 1.660947433286918,
+            'token_entropy': 2.5024938932551737,
+        },
+        {
+            'chosen_entropy': 1.3862943611198906,
+            'prompt_entropy': 1.660947433286918,
+            'rejected_entropy': 1.3862943611198906,
+            'token_entropy': 2.496481079820755,
+        },
+        {
+            'chosen_entropy': 1.3862943611198906,
+            'prompt_entropy': 1.3862943611198906,
+            'rejected_entropy': 1.3862943611198906,
+            'token_entropy': 2.3034884952192693,
+        },
+        {
+            'chosen_entropy': -0.0,
+            'prompt_entropy': 1.660947433286918,
+            'rejected_entropy': -0.0,
+            'token_entropy': 2.289294054503374,
+        },
+        {
+            'chosen_entropy': 1.660947433286918,
+            'prompt_entropy': -0.0,
+            'rejected_entropy': 1.3862943611198906,
+            'token_entropy': 2.465931856301205,
+        },
+    ]
+    assert entropy_validation(dataset) == expected_entropy
