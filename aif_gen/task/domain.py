d9a7fd2bdc675da9776d9d2e83962609300ee5f8
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-01-24 12:33:19 -0500

    Add docstrings

--- aif_gen/task/domain.py
+++ aif_gen/task/domain.py
@@ -6,9 +6,16 @@ from .domain_component import DomainComponent
 class Domain:
     r"""A domain is a combination of DomainComponents along with a weight for each component.
 
+    Note: We do not enforce that the weights be normalized.
+
     Args:
         components (List[DomainComponent]): List of DomainComponents that constitute the domain.
         weights (Optional[List[float]]): Weights given to each constituent component (uniform if not specified).
+
+    Raises:
+        ValueError: If the list of components is empty.
+        ValueError: If the number of weights matches the number of components.
+        ValueError: If any of the provided weights are negative.
     """
 
     def __init__(
@@ -37,6 +44,26 @@ class Domain:
 
     @classmethod
     def from_dict(cls, domain_dict: Dict[str, Dict[str, Any]]) -> 'Domain':
+        r"""Construct an Domain from dictionary represenetation.
+
+        Note:
+            Expects each key to denote the 'name' of a DomainComponent, and each value
+            to be a dictionary that is parsable by DomainComponent.from_dict().
+
+            Moreover, each value should include a ('weight': weight_value: float) key-value
+            pair that encodes the weight for that given DomainComponent.
+
+            If these 'weight' keys are no present, the uniform weight initialization is used.
+
+        Args:
+            domain_dict(Dict[str, Dict[str, Any]]): The dictionary that encodes the Domain.
+
+        Returns:
+            Domain: The newly constructed Domain.
+
+        Raises:
+            ValueError: If the input dictionary is missing any required keys.
+        """
         components, component_weights = [], []
         for component_name, component_args in domain_dict.items():
             component_args['name'] = component_name
@@ -48,6 +75,20 @@ class Domain:
         weights = None if not len(component_weights) else component_weights
         return cls(components, weights)
 
+    def to_dict(self) -> Dict[str, Any]:
+        r"""Convert the Domain to dictionary represenetation.
+
+        Note: This method is the functional inverse of Domain.from_dict().
+
+        Returns:
+            Dict[str, Any]: The dictionary representation of the Domain.
+        """
+        domain_dict = {}
+        for i in range(self.num_components):
+            domain_dict[self.components[i].name] = self.components[i].to_dict()
+            domain_dict[self.components[i].name]['weight'] = self.weights[i]
+        return domain_dict
+
     def __str__(self) -> str:
         s = f'Domain: ['
         for i in range(self.num_components):
@@ -57,12 +98,15 @@ class Domain:
 
     @property
     def components(self) -> List[DomainComponent]:
+        """List[DomainComponent]: The list of components associated with this Domain."""
         return self._components
 
     @property
     def num_components(self) -> int:
+        """int: The number of components associated with this Domain."""
         return len(self.components)
 
     @property
     def weights(self) -> List[float]:
+        """List[float]: The weights associated with this Domain."""
         return self._weights
--- aif_gen/task/domain_component.py
+++ aif_gen/task/domain_component.py
@@ -9,6 +9,9 @@ class DomainComponent:
         name (str): The name that describes the content of the domain component.
         seed_words (List[str]): The list of seed words that describe the domain component.
         description (Optional[str]): An optional description of the domain component.
+
+    Raises:
+        ValueError: If no seed words are provided.
     """
 
     def __init__(
@@ -28,6 +31,20 @@ class DomainComponent:
 
     @classmethod
     def from_dict(cls, component_dict: Dict[str, Any]) -> 'DomainComponent':
+        r"""Construct an AlignmentTask from dictionary represenetation.
+
+        Note:
+            Expects 'name', and 'seed_words' keys to be present in the dictionary.
+
+        Args:
+            component_dict(Dict[str, Any]): The dictionary that encodes the DomainComponent.
+
+        Returns:
+            DomainComponent: The newly constructed DomainComponent.
+
+        Raises:
+            ValueError: If the input dictionary is missing any required keys.
+        """
         expected_keys = {'name', 'seed_words'}
         missing_keys = expected_keys - set(component_dict)
         if len(missing_keys):
@@ -38,11 +55,25 @@ class DomainComponent:
         description = component_dict.get('description')
         return cls(name, seed_words, description)
 
+    def to_dict(self) -> Dict[str, Any]:
+        r"""Convert the DomainComponent to dictionary represenetation.
+
+        Note: This method is the functional inverse of DomainComponent.from_dict().
+
+        Returns:
+            Dict[str, Any]: The dictionary representation of the DomainComponent.
+        """
+        component_dict = {'name': self.name, 'seed_words': self.seed_words}
+        if self.description is not None:
+            component_dict['description'] = self.description
+        return component_dict
+
     def __str__(self) -> str:
         s = f'{self._name} '
         if self.description is not None:
             s += f'({self.description}) '
 
+        # Truncate number of seed words to first 3 to avoid spamming output stream
         if len(self.seed_words) > 3:
             s += str(self._seed_words[:3])[:-1] + ', ...]'
         else:
@@ -52,16 +83,20 @@ class DomainComponent:
 
     @property
     def name(self) -> str:
+        """str: The name of this DomainComponent."""
         return self._name
 
     @property
     def seed_words(self) -> List[str]:
+        """List[str]: The list of seed words aliased by this DomainComponent."""
         return self._seed_words
 
     @property
     def num_seed_words(self) -> int:
+        """int: The number of seed words aliased by this DomainComponent."""
         return len(self.seed_words)
 
     @property
     def description(self) -> Optional[str]:
+        """Optional[str]: The description in the current DomainComponent, if it exists."""
         return self._description
