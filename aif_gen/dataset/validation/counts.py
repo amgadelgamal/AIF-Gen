80961755b8f777ca743776c06727e322a6f2f648
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-01-24 17:08:56 -0500

    Implement count validation

--- aif_gen/dataset/validation/counts.py
+++ aif_gen/dataset/validation/counts.py
@@ -1,5 +1,6 @@
 from typing import Dict, List
 
+from aif_gen.dataset import AlignmentDataset, ContinualAlignmentDataset
 from aif_gen.typing import Dataset
 
 
@@ -22,4 +23,31 @@ def count_validation(dataset: Dataset) -> List[Dict[str, int]]:
         If the input dataset is an AlignmentDataset (non-continual), this function
         returns a 1 element list with the relevant statistics.
     """
-    raise NotImplementedError()
+    if isinstance(dataset, AlignmentDataset):
+        datasets = [dataset]
+    else:
+        # This assert is here to make mypy happy
+        assert isinstance(dataset, ContinualAlignmentDataset)
+        datasets = dataset.datasets
+
+    results = []
+    for dataset in datasets:
+        results.append(_count_validation(dataset))
+    return results
+
+
+def _count_validation(dataset: AlignmentDataset) -> Dict[str, int]:
+    samples, prompts, chosen, rejected = set(), set(), set(), set()
+    for sample in dataset.samples:
+        samples.add(str(sample))
+        prompts.add(sample.prompt)
+        chosen.add(sample.chosen)
+        rejected.add(sample.rejected)
+
+    return {
+        'sample': dataset.num_samples,
+        'unique_samples': len(samples),
+        'unique_prompts': len(prompts),
+        'unique_chosen': len(chosen),
+        'unique_rejected': len(rejected),
+    }
--- test/test_validation/test_count_validation.py
+++ test/test_validation/test_count_validation.py
@@ -1,13 +1,204 @@
-import pytest
-
+from aif_gen.dataset import (
+    AlignmentDataset,
+    AlignmentDatasetSample,
+    ContinualAlignmentDataset,
+)
 from aif_gen.dataset.validation import count_validation
 
 
-@pytest.mark.skip(reason='Count validation not implemented')
-def test_count_validation():
-    _ = count_validation()
+def test_count_validation_all_unique():
+    samples = [
+        AlignmentDatasetSample(
+            'Mock prompt A 1', 'Winning Response A 1', 'Losing Response A 1'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt B 1', 'Winning Response B 1', 'Losing Response B 1'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt C 1', 'Winning Response C 1', 'Losing Response C 1'
+        ),
+    ]
+    mock_task = None
+    dataset = AlignmentDataset(task=mock_task, samples=samples)
+    expected_counts = [
+        {
+            'sample': 3,
+            'unique_samples': 3,
+            'unique_prompts': 3,
+            'unique_chosen': 3,
+            'unique_rejected': 3,
+        }
+    ]
+    assert count_validation(dataset) == expected_counts
 
 
-@pytest.mark.skip(reason='Count validation not implemented')
+def test_count_validation_all_same_prompts():
+    samples = [
+        AlignmentDatasetSample(
+            'Mock prompt A 2', 'Winning Response A 2', 'Losing Response A 2'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt A 2', 'Winning Response B 2', 'Losing Response B 2'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt A 2', 'Winning Response C 2', 'Losing Response C 2'
+        ),
+    ]
+    mock_task = None
+    dataset = AlignmentDataset(task=mock_task, samples=samples)
+    expected_counts = [
+        {
+            'sample': 3,
+            'unique_samples': 3,
+            'unique_prompts': 1,
+            'unique_chosen': 3,
+            'unique_rejected': 3,
+        }
+    ]
+    assert count_validation(dataset) == expected_counts
+
+
+def test_count_validation_all_same_responses():
+    samples = [
+        AlignmentDatasetSample(
+            'Mock prompt A 3', 'Winning Response A 3', 'Losing Response B 3'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt B 3', 'Winning Response A 3', 'Losing Response B 3'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt C 3', 'Winning Response A 3', 'Losing Response B 3'
+        ),
+    ]
+    mock_task = None
+    dataset = AlignmentDataset(task=mock_task, samples=samples)
+    expected_counts = [
+        {
+            'sample': 3,
+            'unique_samples': 3,
+            'unique_prompts': 3,
+            'unique_chosen': 1,
+            'unique_rejected': 1,
+        }
+    ]
+    assert count_validation(dataset) == expected_counts
+
+
+def test_count_validation_all_same_everything():
+    samples = [
+        AlignmentDatasetSample(
+            'Mock prompt A 4', 'Winning Response A 4', 'Losing Response A 4'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt A 4', 'Winning Response A 4', 'Losing Response A 4'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt A 4', 'Winning Response A 4', 'Losing Response A 4'
+        ),
+    ]
+    mock_task = None
+    dataset = AlignmentDataset(task=mock_task, samples=samples)
+    expected_counts = [
+        {
+            'sample': 3,
+            'unique_samples': 1,
+            'unique_prompts': 1,
+            'unique_chosen': 1,
+            'unique_rejected': 1,
+        }
+    ]
+    assert count_validation(dataset) == expected_counts
+
+
 def test_count_countinual_dataset():
-    _ = count_validation()
+    samples = [
+        AlignmentDatasetSample(
+            'Mock prompt A 1', 'Winning Response A 1', 'Losing Response A 1'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt B 1', 'Winning Response B 1', 'Losing Response B 1'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt C 1', 'Winning Response C 1', 'Losing Response C 1'
+        ),
+    ]
+    mock_task = None
+    dataset_one = AlignmentDataset(task=mock_task, samples=samples)
+
+    samples = [
+        AlignmentDatasetSample(
+            'Mock prompt A 2', 'Winning Response A 2', 'Losing Response A 2'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt A 2', 'Winning Response B 2', 'Losing Response B 2'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt A 2', 'Winning Response C 2', 'Losing Response C 2'
+        ),
+    ]
+    mock_task = None
+    dataset_two = AlignmentDataset(task=mock_task, samples=samples)
+
+    samples = [
+        AlignmentDatasetSample(
+            'Mock prompt A 3', 'Winning Response A 3', 'Losing Response B 3'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt B 3', 'Winning Response A 3', 'Losing Response B 3'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt C 3', 'Winning Response A 3', 'Losing Response B 3'
+        ),
+    ]
+    mock_task = None
+    dataset_three = AlignmentDataset(task=mock_task, samples=samples)
+
+    samples = [
+        AlignmentDatasetSample(
+            'Mock prompt A 4', 'Winning Response A 4', 'Losing Response A 4'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt A 4', 'Winning Response A 4', 'Losing Response A 4'
+        ),
+        AlignmentDatasetSample(
+            'Mock prompt A 4', 'Winning Response A 4', 'Losing Response A 4'
+        ),
+    ]
+    mock_task = None
+    dataset_four = AlignmentDataset(task=mock_task, samples=samples)
+
+    dataset = ContinualAlignmentDataset(
+        [dataset_one, dataset_two, dataset_three, dataset_four]
+    )
+
+    expected_counts = [
+        {
+            'sample': 3,
+            'unique_samples': 3,
+            'unique_prompts': 3,
+            'unique_chosen': 3,
+            'unique_rejected': 3,
+        },
+        {
+            'sample': 3,
+            'unique_samples': 3,
+            'unique_prompts': 1,
+            'unique_chosen': 3,
+            'unique_rejected': 3,
+        },
+        {
+            'sample': 3,
+            'unique_samples': 3,
+            'unique_prompts': 3,
+            'unique_chosen': 1,
+            'unique_rejected': 1,
+        },
+        {
+            'sample': 3,
+            'unique_samples': 1,
+            'unique_prompts': 1,
+            'unique_chosen': 1,
+            'unique_rejected': 1,
+        },
+    ]
+    assert count_validation(dataset) == expected_counts
