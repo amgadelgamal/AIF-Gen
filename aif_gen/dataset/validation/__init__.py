eb7a16bf34c5ad09fe3bd9dcef12a9c5dfc6a8e0
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-01-27 14:53:41 -0500

    Remove statistical validator for future work

--- aif_gen/dataset/validation/__init__.py
+++ aif_gen/dataset/validation/__init__.py
@@ -1,3 +1,2 @@
 from aif_gen.dataset.validation.counts import count_validation
 from aif_gen.dataset.validation.entropy import entropy_validation
-from aif_gen.dataset.validation.statistical import drift_detection
--- test/test_validation/test_statistical_validation.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-from aif_gen.dataset.validation import drift_detection
-
-
-@pytest.mark.skip(reason='Drift detection not implemented')
-def test_drift_detection():
-    _ = drift_detection()
