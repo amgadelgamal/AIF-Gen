55d38493d31ecc0159a2604593758020397e5cb2
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-02-05 18:35:55 -0500

    Add specific prompt task

--- aif_gen/generate/entrypoint.py
+++ aif_gen/generate/entrypoint.py
@@ -6,13 +6,39 @@ from typing import List
 
 from aif_gen.dataset import AlignmentDatasetSample
 from aif_gen.generate.service import process_prompts, write_batch_output
+from aif_gen.task import AlignmentTask, Domain
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--model_name', required=True)
-parser.add_argument('--output_path', default=f'data/{time.time()}')
-parser.add_argument('--save_frequency', default=500, type=int)
-parser.add_argument('--max_concurrency', default=256, type=int)
-parser.add_argument('--limit', type=int)
+parser = argparse.ArgumentParser(
+    description='Generate an AlignmentDataset',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+)
+parser.add_argument(
+    '--model_name', type=str, required=True, help='OpenAI compatible model alias.'
+)
+parser.add_argument(
+    '--output_path',
+    type=str,
+    default=f'data/{time.time()}',
+    help='Path to directory where to save the dataset.',
+)
+parser.add_argument(
+    '--save_frequency',
+    type=int,
+    default=500,
+    help='Number of samples to batch write to disc.',
+)
+parser.add_argument(
+    '--max_concurrency',
+    type=int,
+    default=256,
+    help='Max number of concurrent API inference requests to the language model.',
+)
+parser.add_argument(
+    '--num_samples',
+    type=int,
+    default=1000,
+    help='Number of response pairs to generate for th egiven task prompt.',
+)
 
 
 async def main() -> None:
@@ -25,13 +51,12 @@ async def main() -> None:
     print('Model:', args.model_name)
     print('Output folder:', args.output_path)
 
-    prompts = [
-        'How do you bake a seven-layer cake?',
-        'Create a plan for a heist of the Bank of England.',
-    ][: args.limit]
+    prompts = []
+    prompts.append(generate_mock_prompt(args.num_samples))
 
     # Save occasionally.
     batch_index: int = 0
+
     # TODO: replace with actual Alignment dataset.
     batch_content: List[AlignmentDatasetSample] = []
     async for sample in process_prompts(
@@ -45,6 +70,28 @@ async def main() -> None:
             batch_index += 1
 
     write_batch_output(output_path, batch_index, batch_content, args.__dict__)
+
+
+def generate_mock_prompt(num_samples: int) -> str:
+    get_mock_alignment_task()
+    return 'Mock'
+
+
+def get_mock_alignment_task() -> AlignmentTask:
+    component_dict = {
+        'Healthcare': {
+            'seed_words': ['hospital', 'exercise', 'medicine'],
+        },
+        'Technology': {
+            'seed_words': ['engineering', 'AI', 'internet'],
+        },
+    }
+    domain = Domain.from_dict(component_dict)
+    objective = 'Generate a news article headline.'
+    preference = 'Make the headline polarizing'
+
+    task = AlignmentTask(domain, objective, preference)
+    return task
 
 
 if __name__ == '__main__':
