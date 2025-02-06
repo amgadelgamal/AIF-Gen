cc1fd63b578095c0d128613f0847f6f58dc4c86f
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-02-06 09:36:40 -0500

    Add basic task config

--- aif_gen/generate/entrypoint.py
+++ aif_gen/generate/entrypoint.py
@@ -4,22 +4,28 @@ import logging
 import pathlib
 import time
 
+import yaml
+
 from aif_gen.api.prompt_mapper import PromptMapper
 from aif_gen.api.response_mapper import ResponseMapper
 from aif_gen.dataset import AlignmentDataset
 from aif_gen.generate.service import process_prompts
-from aif_gen.task import AlignmentTask, Domain
+from aif_gen.task import AlignmentTask
 from aif_gen.util.logging import setup_basic_logging
+from aif_gen.util.path import get_root_dir
 
 parser = argparse.ArgumentParser(
     description='Generate an AlignmentDataset',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter,
 )
 parser.add_argument(
-    '--model_name', type=str, required=True, help='OpenAI compatible model alias.'
+    '--config_file',
+    type=str,
+    default='config/aif_config.yaml',
+    help='Path to configuration file to use.',
 )
 parser.add_argument(
-    '--log_file_name',
+    '--log_file',
     type=str,
     default=f'aif_generation.log',
     help='Name of the log file to write to within the output directory.',
@@ -42,38 +48,37 @@ parser.add_argument(
     default=256,
     help='Max number of concurrent API inference requests to the language model.',
 )
-parser.add_argument(
-    '--num_samples',
-    type=int,
-    default=1000,
-    help='Number of response pairs to generate for th egiven task prompt.',
-)
 
 
 async def main() -> None:
     args = parser.parse_args()
-    async_semaphore = asyncio.Semaphore(args.max_concurrency)
 
-    output_path = pathlib.Path(args.output_path)
-    output_path.mkdir(parents=True, exist_ok=True)
-    log_file = output_path / args.log_file_name
+    log_file = get_root_dir() / args.log_file
     setup_basic_logging(log_file)
 
-    task = get_mock_alignment_task()
+    config_file = get_root_dir() / args.config_file
+    config_dict = yaml.safe_load(config_file.read_text())
+    logging.info(f'Using configuration: {config_dict}')
 
+    output_path = get_root_dir() / args.output_path
+    output_path.mkdir(parents=True, exist_ok=True)
+
+    async_semaphore = asyncio.Semaphore(args.max_concurrency)
+
+    task = AlignmentTask.from_dict(config_dict['alignment_task'])
     logging.info(f'Generating AIF Dataset for task: {task}')
-    logging.info(f'Using Model: {args.model_name}')
+    logging.info(f'Using Model: {config_dict["model_name"]}')
 
-    prompts = []
-    task_prompt = generate_mock_task_prompt(task)
-    response_prompt = generate_mock_response_prompt(task, task_prompt, args.num_samples)
-    prompts.append(task_prompt)
-    prompts.append(response_prompt)
+    # TODO: Technically these need to be done sequentially
+    task_prompt = get_task_prompt(task)
+    response_prompt = get_response_prompt(task, task_prompt, config_dict['num_samples'])
 
     batch_index: int = 0
     dataset = AlignmentDataset(task, [])
     async for sample in process_prompts(
-        prompts, model_name=args.model_name, async_semaphore=async_semaphore
+        prompts=[task_prompt, response_prompt],
+        model_name=config_dict['model_name'],
+        async_semaphore=async_semaphore,
     ):
         if sample is not None:
             dataset.append(sample)
@@ -96,7 +101,7 @@ def write_dataset_batch(
     logging.info(f'Wrote batch with {len(dataset)} samples to {output_file_path}')
 
 
-def generate_mock_task_prompt(task: AlignmentTask) -> str:
+def get_task_prompt(task: AlignmentTask) -> str:
     logging.info(f'Generating task prompt for {task}')
 
     prompt_mapper = PromptMapper()
@@ -105,33 +110,13 @@ def generate_mock_task_prompt(task: AlignmentTask) -> str:
     return prompt
 
 
-def generate_mock_response_prompt(
-    task: AlignmentTask, task_prompt: str, num_samples: int
-) -> str:
+def get_response_prompt(task: AlignmentTask, task_prompt: str, num_samples: int) -> str:
     logging.info(f'Generating response prompt ({num_samples} samples)')
 
     response_mapper = ResponseMapper()
     prompt = response_mapper.generate_prompt(task, task_prompt, num_samples=num_samples)
     logging.info(f'Using response prompt:\n\n{prompt}')
     return prompt
-
-
-def get_mock_alignment_task() -> AlignmentTask:
-    logging.info('Generating mock alignment task')
-    component_dict = {
-        'Healthcare': {
-            'seed_words': ['hospital', 'exercise', 'medicine'],
-        },
-        'Technology': {
-            'seed_words': ['engineering', 'AI', 'internet'],
-        },
-    }
-    domain = Domain.from_dict(component_dict)
-    objective = 'Generate a news article headline.'
-    preference = 'Make the headline polarizing'
-
-    task = AlignmentTask(domain, objective, preference)
-    return task
 
 
 if __name__ == '__main__':
