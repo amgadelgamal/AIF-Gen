c64f1f66e4841d6751823bc11e92837bc558cf4c
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2025-02-13 11:52:20 -0500

    Add seed word alias initialization to DomainComponent

--- test/test_domain_component.py
+++ test/test_domain_component.py
@@ -1,6 +1,7 @@
 import pytest
 
 from aif_gen.task import DomainComponent
+from aif_gen.task.seed_words import get_seed_words
 
 
 def test_init():
@@ -31,6 +32,21 @@ def test_init_empty_seed_words():
     seed_words = []
     with pytest.raises(ValueError):
         _ = DomainComponent(name, seed_words)
+
+
+@pytest.mark.parametrize(
+    'seed_word_alias', ['education', 'finance', 'healthcare', 'politics', 'technology']
+)
+def test_init_seed_word_alias(seed_word_alias):
+    name = 'TestComponent'
+    description = 'Mock Domain Component'
+
+    component = DomainComponent(name, seed_word_alias, description)
+    exp_seed_words = get_seed_words(seed_word_alias)
+    assert component.name == name
+    assert component.seed_words == exp_seed_words
+    assert component.description == description
+    assert component.num_seed_words == len(exp_seed_words)
 
 
 def test_init_from_dict():
