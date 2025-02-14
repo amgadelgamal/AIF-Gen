92aa7d2c905d37827294a996098be276bfb9e550
Taz <tazscotttalib@gmail.com>
2025-02-13 20:05:43 -0500

    Add response diversity metric (inverse Self-BLEU score)

--- aif_gen/dataset/validation/metrics_evaluator.py
+++ aif_gen/dataset/validation/metrics_evaluator.py
@@ -1,8 +1,15 @@
 import spacy
 import torch
+import nltk
 from sentence_transformers import SentenceTransformer, util
 from transformers import GPT2LMHeadModel, GPT2TokenizerFast, pipeline
+from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+from multiprocessing import Pool
 
+def calc_bleu(response, hypothesis, weight) -> float:
+        """Computes the BLEU score for a single response against others.
+        Needs to be declared outside of class for parallel processing."""
+        return sentence_bleu(response, hypothesis, weight, smoothing_function=SmoothingFunction().method1)
 
 class MetricsEvaluator:
     def __init__(self) -> None:
@@ -79,3 +86,50 @@ class MetricsEvaluator:
         score_chosen = self.compute_response_alignment_score(chosen_response)
         score_rejected = self.compute_response_alignment_score(rejected_response)
         return score_chosen - score_rejected
+
+    def compute_response_diversity(
+        self, response_set: list[str], ngram: int = 3, parallel: bool = False
+    ) -> float:
+        """
+        Computes the diversity for a set of responses via the inverse Self-BLEU score.
+        A higher Self-BLEU score indicates lower diversity for generated sentences,
+        therefore we return the inverse score.
+        https://arxiv.org/pdf/1802.01886
+        https://github.com/geek-ai/Texygen
+        
+        Args:
+            response_set (list[str]): A list of generated sentences.
+            ngram (int): The maximum n-gram order for BLEU calculation. Default of 3 matches the original paper.
+            parallel (bool): Whether to compute BLEU scores in using Pool multiprocessing.
+        """
+        
+        # Avoid redundant calculations
+        if not response_set or len(response_set) < 2:
+            return 0.0
+        
+        # BLEU weight setting (e.g., for BLEU-3: (1/3, 1/3, 1/3))
+        weight = tuple(1. / ngram for _ in range(ngram))
+        
+        # Tokenize responses for BLEU calculation
+        tokenized_responses = [nltk.word_tokenize(sentence) for sentence in response_set]
+
+        scores = []
+
+        if parallel:
+            # Compute BLEU scores in parallel
+            with Pool() as pool:
+                scores = pool.starmap(calc_bleu, 
+                                [(tokenized_responses[:i] + tokenized_responses[i + 1:], hypothesis, weight) 
+                                for i, hypothesis in enumerate(tokenized_responses)])
+        else:
+            # Compute BLEU scores sequentially
+            for i, hypothesis in enumerate(tokenized_responses):
+                other_responses = tokenized_responses[:i] + tokenized_responses[i + 1:]
+                score = calc_bleu(other_responses, hypothesis, weight)
+                scores.append(score)
+
+        # Average self-BLEU score
+        bleu_score = sum(scores) / len(scores)
+
+        # Return the inverse BLEU score as diversity metric
+        return 1.0 - bleu_score
