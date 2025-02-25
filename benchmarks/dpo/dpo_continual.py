76c53a51fe295466a69d8145cc191fac8b3c15b1
Shahrad <77243080+EMZEDI@users.noreply.github.com>
2025-02-25 09:19:40 -0500

    Update dpo_continual.py
    Co-authored-by: Jacob Chmura <jacobpaul.chmura@gmail.com>

--- benchmarks/dpo/dpo_continual.py
+++ benchmarks/dpo/dpo_continual.py
@@ -135,7 +135,6 @@ def main(
         script_args.dataset_name
     )
     output_dir = training_args.output_dir
-    # TODO remove the check once ruff issues are resolved
 
     if training_args.reward_model_path is not None:
         for i, _ in enumerate(continual_dataset):
