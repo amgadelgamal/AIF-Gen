13cd3d1db49defc7894493435a25ff380289801b
Jacob Chmura <jacobpaul.chmura@gmail.com>
2025-04-12 11:04:17 -0400

    Transmute (#125)
    * Transmute
    
    * CLI Typos

--- aif_gen/generate/service.py
+++ aif_gen/generate/service.py
@@ -275,3 +275,194 @@ async def _generate_sample(
     except pydantic.ValidationError as e:
         logging.error(f'Failed to bind structured output json schema: {e}')
         return None
+
+
+async def transmute_continual_dataset(
+    input_dataset: ContinualAlignmentDataset,
+    model_name: str,
+    client: openai.AsyncOpenAI,
+    async_semaphore: asyncio.Semaphore,
+    max_tokens_chosen_rejected_response: int = 2048,
+    dry_run: bool = False,
+) -> Optional[ContinualAlignmentDataset]:
+    r"""Overriding the 'rejected' responses with the 'chosen' response of a new model.
+
+    Args:
+        input_dataset (ContinualAlignmentDataset): Dataset to transmute.
+        model_name (str): The vLLM-compatible model alias to use for generation synthetic samples.
+        client (openai.AsyncOpenAI): Handle to openAI client.
+        async_semaphore (asyncio.Semaphore): Semaphore that manages number of concurrent API requests.
+        max_tokens_chosen_rejected_response (int): Configurable limit on the max_tokens for the generated chosen and rejected response.
+        dry_run (bool): If True, ignore the config and generate a dummy sample to ensure the model is setup correctly.
+
+    Returns:
+        Optional[ContinualAlignmentDataset]: The synthetically generated dataset.
+    """
+    response_mapper = ResponseMapper()
+
+    if dry_run:
+        logging.info(f'Doing dry-run data transmutation on a single sample...')
+        mock_task = input_dataset.datasets[0].task
+        mock_sample = input_dataset[0]
+        cache = await AsyncElasticsearchCache.maybe_from_env_var(
+            index_name=f'CACHE_DATA_TRANSMUTATION_{model_name}'
+        )
+        coro = _transmute_sample(
+            mock_task,
+            mock_sample,  # type: ignore
+            client,
+            model_name,
+            response_mapper,
+            async_semaphore,
+            max_tokens_chosen_rejected_response,
+            dataset_idx=-1,
+            cache=cache,
+        )
+        try:
+            _ = await coro
+        except BaseException as e:
+            logging.exception(f'Exception occured on dry-run, skipping transmute: {e}')
+            raise e
+        finally:
+            if cache is not None:
+                await cache.close()
+
+        logging.info('Dry run was a success.')
+        return None
+
+    cache = await AsyncElasticsearchCache.maybe_from_env_var(
+        index_name=f'CACHE_DATA_TRANSMUTATION_{model_name}'
+    )
+    futures, tasks, dataset_sizes = [], [], []
+    for dataset_idx, dataset in enumerate(input_dataset.datasets):
+        task = dataset.task
+        dataset_size = len(dataset)
+        logging.info(f'Transmuting Dataset ({dataset_size} samples) {task}')
+
+        tasks.append(task)
+        dataset_sizes.append(dataset_size)
+        for _sample_idx in range(dataset_size):
+            coro = _transmute_sample(
+                task,
+                dataset[_sample_idx],  # type: ignore
+                client,
+                model_name,
+                response_mapper,
+                async_semaphore,
+                max_tokens_chosen_rejected_response,
+                dataset_idx=dataset_idx,
+                cache=cache,
+            )
+            futures.append(asyncio.create_task(coro))
+
+    try:
+        samples: List[List[AlignmentDatasetSample]] = [
+            [] for _ in range(len(dataset_sizes))
+        ]
+        for fut in tqdm.as_completed(futures, total=len(futures)):
+            result = await fut
+            if result is not None:
+                sample, dataset_idx = result
+                samples[dataset_idx].append(sample)
+
+        continual_dataset = ContinualAlignmentDataset(datasets=[])
+        for i in range(len(samples)):
+            if len(samples[i]) != dataset_sizes[i]:
+                logging.warning(
+                    f'Dataset {i} requested {dataset_sizes[i]} samples but LM generated {len(samples[i])}'
+                )
+            continual_dataset.append(AlignmentDataset(tasks[i], samples[i]))
+        return continual_dataset
+    except BaseException as e:
+        logging.exception(f'Exception occured while generating dataset: {e}')
+        for fut in futures:
+            fut.cancel()
+        await tqdm.gather(*futures)
+        return None
+    finally:
+        if cache is not None:
+            await cache.close()
+
+
+@backoff.on_exception(
+    backoff.expo,
+    (openai.RateLimitError, openai.InternalServerError, openai.APITimeoutError),
+    max_tries=get_tries(),
+)
+async def _transmute_sample(
+    task: AlignmentTask,
+    sample: AlignmentDatasetSample,
+    client: openai.AsyncOpenAI,
+    model_name: str,
+    response_mapper: ResponseMapper,
+    async_semaphore: asyncio.Semaphore,
+    max_tokens_chosen_rejected_response: int,
+    dataset_idx: int,
+    cache: 'AsyncElasticsearchCache | None' = None,
+) -> Optional[Tuple[AlignmentDatasetSample, int]]:
+    r"""Transmute an AlignmentDatasetSample given the model.
+
+    Args:
+        task (AlignmentTask): The AlignmentTask to transmute.
+        sample (AlignmentDatasetSample): The AlignmentDatasetSample to transmute.
+        client (openai.AsyncOpenAI): Handle to openAI client.
+        model_name (str): openAI model alias.
+        response_mapper (ResponseMapper): Created the 'meta-prompt' for this sample's response prompt.
+        async_semaphore (asyncio.Semaphore): Semaphore that manages number of concurrent API requests.
+        max_tokens_chosen_rejected_response (int): Configurable limit on the max_tokens for the generated chosen and rejected response.
+        dataset_idx (int): The idx of the dataset that the sample is requested for to align out-of-order asyn execution.
+        max_tokens (int): Max number of tokens to generate.
+        cache (AsyncElasticsearchCache): Optionally specify a AsyncElasticsearchCache instance for caching.
+
+    Returns:
+        Optional[Tuple[AlignmentDatasetSample, int]]: A single sample of the dataset, and the dataset idx (None if pydantic.ValidationError occurred).
+
+    Raises:
+        openai.NotFoundError: If the openAI model cannot be accessed at the configured endpoint.
+    """
+    try:
+
+        class _ResponsePair(pydantic.BaseModel, extra='forbid'):
+            chosen: str
+            rejected: str
+
+        prompt = sample.prompt
+        task_prompt = response_mapper.generate_prompt(task, prompt)
+
+        async with async_semaphore:
+            if cache is not None:
+                output = await cache.get(task_prompt)
+            else:
+                output = None
+
+            if output is None:
+                response = await client.chat.completions.create(
+                    model=model_name,
+                    messages=[{'role': 'user', 'content': task_prompt}],
+                    max_tokens=max_tokens_chosen_rejected_response,
+                    response_format={
+                        'type': 'json_schema',
+                        'json_schema': {
+                            'name': 'SyntheticPreference',
+                            'schema': _ResponsePair.model_json_schema(),
+                            'strict': True,
+                        },
+                    },
+                )
+                output = response.choices[0].message.content
+                assert output is not None  # This is for mypy
+
+        if prompt is None:
+            raise ValueError(f'Received None response to prompt: {prompt}')
+
+        structured_response = _ResponsePair.model_validate_json(output)
+        if cache is not None:
+            await cache.set(query=task_prompt, value=output)
+
+        # The models 'chosen' response becomes the samples 'rejected' attribute
+        sample.rejected = structured_response.chosen
+        logging.debug(f'Task Prompt: {task_prompt}, Sample: {sample}')
+        return sample, dataset_idx
+    except pydantic.ValidationError as e:
+        logging.error(f'Failed to bind structured output json schema: {e}')
+        return None
